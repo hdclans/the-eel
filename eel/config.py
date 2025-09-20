9d6f20f0d3eabe7eda7e22e532d1e353e467ca40
hdclans <hugo1daugeron@icloud.com>
2025-09-21 01:10:42 +0200

    grille

--- eel/config.py
+++ eel/config.py
@@ -3,12 +3,16 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
 # Couleurs
-BG_COLOR = "purple"
-PLAYER_COLOR = "red"
+BG_COLOR = "darkblue"
+PLAYER_COLOR = "darkgreen"
 
 # Joueur
-PLAYER_RADIUS = 50
+PLAYER_RADIUS = 25
 PLAYER_SPEED = 300
 
 # FPS
 FPS = 60
+
+# Grille
+GRID_COLOR = "darkred"
+CELL_SIZE = 55
--- eel/game.py
+++ eel/game.py
@@ -4,26 +4,31 @@ from . import config
 class Game:
     def __init__(self):
         pygame.init()
+        # Création de la fenêtre principale avec largeur et hauteur définies dans config
         self.screen = pygame.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
-        pygame.display.set_caption("The eel")
-        self.clock = pygame.time.Clock()
-        self.running = True
-        self.dt = 0
+        pygame.display.set_caption("The eel")  # Titre de la fenêtre
+        self.clock = pygame.time.Clock()       # Horloge pour contrôler le framerate
+        self.running = True                    # Booléen pour garder la boucle principale active
+        self.dt = 0                            # Delta time = temps écoulé entre deux frames
 
-        self.player_pos = pygame.Vector2(
+        # Centre de l’écran
+        self.center = pygame.Vector2(
             self.screen.get_width() / 2, 
             self.screen.get_height() / 2
         )
 
+        self.player_pos = pygame.Vector2(self.screen.get_width() / 2 - 164, self.screen.get_height() / 2 + 1)
+
     def run(self):
         while self.running:
-            self.handle_events()
-            self.update()
-            self.draw()
+            self.handle_events()   # Gestion des événements (clavier, souris, fermeture fenêtre)
+            self.update()          # Logique du jeu (mouvements, collisions...)
+            self.draw()            # Rendu graphique
 
+            # Calcul du delta time (temps entre deux frames)
             self.dt = self.clock.tick(config.FPS) / 1000
 
-        pygame.quit()
+        pygame.quit()              # Quitter pygame
 
     def handle_events(self):
         for event in pygame.event.get():
@@ -45,6 +50,29 @@ class Game:
             self.player_pos.x += config.PLAYER_SPEED * self.dt
 
     def draw(self):
-        self.screen.fill(config.BG_COLOR)
-        pygame.draw.circle(self.screen, config.PLAYER_COLOR, self.player_pos, config.PLAYER_RADIUS)
-        pygame.display.flip()
+        self.screen.fill(config.BG_COLOR)  # Efface l’écran avec la couleur de fond
+        self.grid(config.CELL_SIZE)        # Dessine une grille centrée        
+        pygame.draw.circle(self.screen, config.PLAYER_COLOR, self.player_pos, config.PLAYER_RADIUS) # Dessine l'anguille
+        pygame.display.flip()              # Met à jour l’affichage
+
+    def grid(self, cell_size):
+        rect_width = 605
+        rect_height = 605
+
+        # Définition d’un rectangle centré
+        rect = pygame.Rect(0, 0, rect_width, rect_height)
+        rect.center = self.center
+
+        # Lignes verticales
+        for x in range(rect.left, rect.right + 1, cell_size):
+            pygame.draw.line(
+                self.screen, "darkgrey",
+                (x, rect.top), (x, rect.bottom), 1
+            )
+
+        # Lignes horizontales
+        for y in range(rect.top, rect.bottom  + 1, cell_size):
+            pygame.draw.line(
+                self.screen, "darkgrey",
+                (rect.left, y), (rect.right, y), 1
+            )
